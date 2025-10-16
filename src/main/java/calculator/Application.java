eef49b46328130009c11acbc1b6999f0def05529
MoseT1 <kimmose981214@gmail.com>
2025-10-16 11:34:46 +0900

    feat(calculator): 덧셈 기능 메서드 구현
    List<String> 형식의 입력을 받으면 숫자로 변환하며 더한 값을 반환하는 addNums() 메서드 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,20 +1,42 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Application {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        String text = sc.next();
 
-        String[] splittedString = splitText(text);
+        String text = Console.readLine();
+
+        List<String> splittedString = splitText(text);
+
+        int sum = addNums(splittedString);
+
+        System.out.println(sum);
+
+
 
     }
 
-    private static String[] splitText(String text) {
+    private static List<String> splitText(String text) {
 
-        return text.split("[,:]");
+        return Arrays.asList(text.split("[,:]"));
 
+    }
+
+    private static int addNums(List<String> list ) {
+
+        int sum = 0;
+
+        for ( String num: list) {
+
+            int temp = Integer.valueOf(num);
+            sum += temp;
+        }
+
+        return sum;
     }
 }
